0cb04c2a7a8484259fb22d2328fd5c72412809c0
23po <ckodalo@gmail.com>
2023-11-05 03:52:14 +0300

    edit

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ const os = require('os');
 
 console.log("Moringa-Devops Test server starting...");
 
+var requestCount = 0;
+
 var handler = function(request, response) {
     console.log("Received request from " + request.connection.remoteAddress);
+    requestCount++;
+    if (requestCount > 5) {
+        response.writeHead(500);
+        response.end("This particular Moringa school server is not okay. Please restart me! I'm server number 6/6");
+        return;
+    }
     response.writeHead(200);
-    response.end("You've hit " + os.hostname() + "\n");
+    response.end("This Moringa school test server is okay, keep testing" + os.hostname() + "\n");
 };
 
 var www = http.createServer(handler);
